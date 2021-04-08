ee3ffe59dc2105acb47637b4ce8119cac6771711
ShouryaSharma17 <shouryasharma17@gmail.com>
2021-04-08 18:03:26 +0530

    Used Switch Case

--- EmpWage.java
+++ EmpWage.java
@@ -1,25 +1,31 @@
 import java.util.*;
 
 public class EmpWage {
-	public static void main(String args[]) {
+    public static void main(String args[]) {
         Random random = new Random();
-        int ranNum = random.nextInt(2);
-        int EMP_IS_PRESENT = 1;
+        int ranNum = random.nextInt(3);
+        //int EMP_IS_PRESENT = 1;
+        // int PART_TIME = 2;
+        int PART_TIME_HR = 4;
         int WAGE_PER_HR = 20;
-        int salary = 0;
         int FULL_DAY_HR = 8;
+        int empHrs=0;
 
-        if (ranNum == 0) {
-                System.out.println("employee is absent");
-            } else {
-                System.out.println("employee is present");
-            }
-        if (ranNum == EMP_IS_PRESENT) {
-            salary = WAGE_PER_HR * FULL_DAY_HR;
-            System.out.println("salary for the day is " + salary);
-        } else {
-            salary = 0;
-            System.out.println("salary for the day is " + salary);
+        switch (ranNum) {
+            case 1:
+                empHrs=FULL_DAY_HR;
+                break;
+
+            case 2:
+                empHrs=PART_TIME_HR;
+                break;
+
+            default:
+                empHrs=0;
+                break;
         }
+        int salary = empHrs * WAGE_PER_HR;
+        System.out.println("salary for the day is " + salary);
     }
 }
+
