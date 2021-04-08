3fddbe8cdad624a12e2f96721f549511f0d854b8
ShouryaSharma17 <shouryasharma17@gmail.com>
2021-04-08 18:18:22 +0530

    Wages for a month

--- EmpWage.java
+++ EmpWage.java
@@ -3,29 +3,38 @@ import java.util.*;
 public class EmpWage {
     public static void main(String args[]) {
         Random random = new Random();
-        int ranNum = random.nextInt(3);
         //int EMP_IS_PRESENT = 1;
         // int PART_TIME = 2;
         int PART_TIME_HR = 4;
         int WAGE_PER_HR = 20;
         int FULL_DAY_HR = 8;
-        int empHrs=0;
+        int WORKING_DAYS = 20;
+        int empHr = 0;
+        int sum = 0;
+        int totalWageOfMonth = 0;
 
-        switch (ranNum) {
-            case 1:
-                empHrs=FULL_DAY_HR;
-                break;
+        int salary = 0;
+        for (int i = 1; i <= WORKING_DAYS; i++) {
+            int ranNum = random.nextInt(3);
 
-            case 2:
-                empHrs=PART_TIME_HR;
-                break;
+            switch (ranNum) {
+                case 1:
+                    empHr = FULL_DAY_HR;
+                    break;
 
-            default:
-                empHrs=0;
-                break;
+                case 2:
+                    empHr = PART_TIME_HR;
+                    break;
+
+                default:
+                    empHr = 0;
+                    break;
+            }
+            salary = empHr * WAGE_PER_HR;
+            totalWageOfMonth+=salary;
+            System.out.println("salary for the day is " + salary);
         }
-        int salary = empHrs * WAGE_PER_HR;
-        System.out.println("salary for the day is " + salary);
+        System.out.println("salary for the month is " + totalWageOfMonth);
     }
 }
 
