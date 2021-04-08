428638274c5061696577d3e445fcba770bf852a4
ShouryaSharma17 <shouryasharma17@gmail.com>
2021-04-08 15:17:53 +0530

    Welcome message will be displayed

---
+++ EmpWage.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class EmpWage {
+
+    public static void main(String[] args) {
+            System.out.println("Welcome to EmployeeWage Assignment");
+        
+    }
+}
