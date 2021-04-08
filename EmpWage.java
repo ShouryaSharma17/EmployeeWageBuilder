5596ac29623f5d14df108f5457e85b2a2f3f0219
ShouryaSharma17 <shouryasharma17@gmail.com>
2021-04-08 15:23:00 +0530

    Employee is absent or present

--- EmpWage.java
+++ EmpWage.java
@@ -3,7 +3,15 @@ import java.util.*;
 public class EmpWage {
 
     public static void main(String[] args) {
-            System.out.println("Welcome to EmployeeWage Assignment");
-        
+        {
+            Random random = new Random();
+            int Num = random.nextInt(2);
+
+            if (Num == 0) {
+                System.out.println("employee is absent");
+            } else {
+                System.out.println("employee is present");
+            }
+        }
     }
 }
