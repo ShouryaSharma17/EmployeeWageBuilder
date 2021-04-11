ff94242b7733d371566fb805bbd08a2bc1d5f283
ShouryaSharma17 <shouryasharma17@gmail.com>
2021-04-11 17:49:53 +0530

    Use of array to manage wage of multiple companies

--- EmpWage.java
+++ EmpWage.java
@@ -1,112 +1,117 @@
-import java.util.*;
-
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class EmpWage {
 
     public static void main(String[] args) {
-        // Creating an Instance of class Employee_check
-        Employee_check first_emp = new Employee_check();
-        Employee_check walmart_emp = new Employee_check();
-	Employee_check apple_emp = new Employee_check();
-        Employee_check tesla_emp = new Employee_check();
-	
-	//Using instance varables to calculate wages
-        first_emp.company_name = "AMAZON";
-        first_emp.working_hours = 10;
-        first_emp.part_time_hrs = 7;
-        first_emp.wage_per_hr = 35;
+        array_wage builder = new array_wage();
+        builder.wage_array_injection("Tesla",14,7,40);
+        builder.wage_array_injection("Amazon",15,8,50);
+        builder.wage_array_injection("Apple",12,5,83);
+        builder.wage_array_injection("Netflix",10,4,68);
+        builder.wage_array_injection("Deep Mind",16,9,72);
 
-        walmart_emp.company_name = "WALMART";
-        walmart_emp.working_hours = 14;
-        walmart_emp.part_time_hrs = 9;
-        walmart_emp.wage_per_hr = 25;
+        builder.print_array_wage();
 
-        apple_emp.company_name = "APPLE";
-        apple_emp.working_hours = 9;
-        apple_emp.part_time_hrs = 5;
-        apple_emp.wage_per_hr = 65;
-
-        tesla_emp.company_name = "TESLA";
-        tesla_emp.working_hours = 7;
-        tesla_emp.part_time_hrs = 3;
-        tesla_emp.wage_per_hr = 45;
-
-
-        // Method Calling
-        System.out.println("FIRST EMPLOYEE :");
-        first_emp.attendance();
-        System.out.println("WALMART EMPLOYEE:");
-        walmart_emp.attendance();
-        System.out.println("APPLE EMPLOYEE:");
-        apple_emp.attendance();
-        System.out.println("TESLA EMPLOYEE:");
-        tesla_emp.attendance();
 
     }
-
-
-    public static class Employee_check {
-
-        String company_name;
-        int working_hours;
-        int work_days = 0;
-        int absent_counter = 0;
-        int part_time_counter = 0;
-        int full_time_counter = 0;
-        int wage_per_hr;
-        //Initializing variable (part time hours)
-        int part_time_hrs;
-        int wage_today = 0;
-        int total_work_hrs = 0;
-
-
-        Random check = new Random();
-
-        //Defining attendance method
-        public void attendance() {
-
-            //int checker =check.nextInt(3);
-
-            while (total_work_hrs < 100 && work_days < 20) {
-
-                int checker = check.nextInt(3);
-                // Using switch to mark attendance
-                switch (checker) {
-                    // Absent case
-                    case 0:
-                        absent_counter += 1;
-                        break;
-                    // Full time Case
-                    case 1:
-                        wage_today = working_hours * wage_per_hr;
-                        total_work_hrs += 8;
-                        full_time_counter += 1;
-                        work_days += 1;
-                        break;
-                    // Part time Case
-                    case 2:
-                        wage_today = working_hours * wage_per_hr;
-                        total_work_hrs += 4;
-                        part_time_counter += 1;
-                        work_days += 1;
-                        break;
-                    default:
-                        break;
-                }
-
-
-            }
-
-            int wage_total = total_work_hrs * wage_per_hr;
-            System.out.println("THE EMPLOYE WORKED FOR " + work_days + " DAYS AND " + total_work_hrs + " HOURS");
-            System.out.println("THE EMPLOYE WAS ABSENT FOR " + absent_counter + " DAYS");
-            System.out.println("THE EMPLOYEE WORKED " + part_time_counter + " PART TIME DAYS");
-            System.out.println("THE EMPLOYEE WORKED " + full_time_counter + " FULL TIME DAYS");
-            System.out.println("THE TOTAL WAGE IN THIS MONTH IS " + wage_total + "\n");
-
-        }
-
-    }
-
 }
+    //class instance
+  class array_wage {
+     int array_counter = 0;
+     Employee_check[] wage_array;
+        // Initializing array
+     public void array_wage() {
+         wage_array = new Employee_check[10];
+     }
+        // Use of INJECTION to enter array elements
+     public void wage_array_injection(String company_name, int working_hours, int part_time_hrs, int wage_per_hr) {
+
+         wage_array[array_counter] = new Employee_check(company_name, working_hours, part_time_hrs, wage_per_hr);
+
+         array_counter++;
+
+     }
+    // Printing array
+     public void print_array_wage() {
+         for (int i =0 ; i < array_counter ;i++) {
+
+             System.out.println(wage_array[i].attendance());
+
+         }
+     }
+
+
+ }
+
+     class Employee_check {
+         String company_name;
+         int working_hours;
+         int work_days;
+         int absent_counter;
+         int part_time_counter;
+         int full_time_counter;
+         int wage_per_hr;
+         //Initializing variable(part time hour)
+         int part_time_hrs;
+         int wage_today;
+         int total_work_hrs;
+         Random check = new Random();
+
+         Employee_check(String company_name, int a, int b, int c) {
+             this.company_name = company_name;
+
+             this.working_hours = a;
+             this.work_days = 0;
+             this.absent_counter = 0;
+             this.part_time_counter = 0;
+             this.full_time_counter = 0;
+             this.wage_per_hr = c;
+             this.part_time_hrs = b;
+             this.total_work_hrs = 0;
+             this.wage_today = 0;
+         }
+
+         //Defining method
+         public List<Serializable> attendance() {
+
+             //int checker =check.nextInt(3);
+
+             while (total_work_hrs < 100 && work_days < 20) {
+
+                 int checker = check.nextInt(3);
+                 // Switch case to mark attendance
+                 switch (checker) {
+                 	case 0:
+                         absent_counter += 1;
+                         break;
+                    case 1:
+                         wage_today = working_hours * wage_per_hr;
+                         total_work_hrs += 8;
+                         full_time_counter += 1;
+                         work_days += 1;
+                         break;
+                   case 2:
+                         wage_today = working_hours * wage_per_hr;
+                         total_work_hrs += 4;
+                         part_time_counter += 1;
+                         work_days += 1;
+                         break;
+                     default:
+                         break;
+                 }
+
+
+             }
+
+             int wage_total = total_work_hrs * wage_per_hr;
+             // Using aslist method to return name and integer
+             return Arrays.asList(company_name,wage_total);
+
+         }
+
+     }
+
 
